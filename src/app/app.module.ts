20077486896b77e3826643c9788755370bc1a21c
kaustubhg99 <kaustubhg99@gmail.com>
2020-09-24 11:19:07 +0530

    added navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import {Routes, RouterModule} from '@angular/router';
+import { HomeComponent } from './home/home.component'
+const appRoutes:   Routes = [
+  {path: '', component:HomeComponent},
+  {path: 'gallery', component:GalleryComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    GalleryComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
